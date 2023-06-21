e5243eb9bf8c8676e4c088954e00312057515e9e
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-21 10:59:19 -0400

    add grocery item response dto to grocery app shared dto

---
+++ Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  GroceryItemResponseDTO.swift
+//  
+//
+//  Created by Thomas Cowern on 6/21/23.
+//
+
+import Foundation
+
+public struct GroceryItemResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(id: UUID, title: String, price: Double, quantity: Int) {
+        self.id = id
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+    
+}
