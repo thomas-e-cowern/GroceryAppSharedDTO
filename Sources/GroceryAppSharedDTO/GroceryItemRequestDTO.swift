22291a84969950215cfecb98af2e8e520215e461
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-21 10:55:21 -0400

    add grocery item request dto to grocery app shared dto

---
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -0,0 +1,21 @@
+//
+//  GroceryItemRequestDTO.swift
+//  
+//
+//  Created by Thomas Cowern on 6/21/23.
+//
+
+import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+    
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(title: String, price: Double, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+}
