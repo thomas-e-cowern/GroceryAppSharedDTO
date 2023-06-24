8439be424060d0431a44278140e893eabab2e74f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-24 14:38:26 -0400

    add CategoryAndItensResponseDTO

---
+++ Sources/GroceryAppSharedDTO/CategoryAndItemsResponseDTO.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryAndItemsResponseDTO.swift
+//  
+//
+//  Created by Thomas Cowern on 6/24/23.
+//
+
+import Foundation
+
+import Foundation
+
+public struct CategoryAndItemsResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let colorCode: String
+    public let items: [GroceryItemResponseDTO]
+    
+    public init(id: UUID, title: String, colorCode: String, items: [GroceryItemResponseDTO]) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+        self.items = items
+    }
+    
+}
